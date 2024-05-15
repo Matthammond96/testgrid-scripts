b4ddd7b0044f552fbcdec546c44591cd299b821e
Matthew Hammond <ultramatt96@hotmail.co.uk>
2024-05-15 10:53:18 +0100

    add force stop of podman and node to cleanup

--- testgrid-cleanup.sh
+++ testgrid-cleanup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+docker rm --force podman nosana-node
 docker system prune -a -f
 docker volume prune -a -f
 docker image prune -a -f
 docker image rm nosana/nosana-cli -f
+docker image rm nosana/podman -f
 
 if cat /proc/version | grep -q 'WSL2'; then
   podman system prune
